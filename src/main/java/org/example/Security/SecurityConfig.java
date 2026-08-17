f8ada4813a568190464f8ada57d3b4b16822bd41
Єгор Непосидяка <yehor.neposydiaka@gmail.com>
2026-08-17 15:40:35 +0300

    security changes

--- src/main/java/org/example/Security/SecurityConfig.java
+++ src/main/java/org/example/Security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
